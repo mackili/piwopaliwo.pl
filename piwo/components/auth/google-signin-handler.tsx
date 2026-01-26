bc617477b79316d943d3f3a1b0790e8c66f486d9
mkilijanski-newdatalabs <maciek.kilijanski@gmail.com>
2026-01-26 23:51:43 +0100

    bugfix: added dynamic url to auth handler

--- piwo/components/auth/google-signin-handler.tsx
+++ piwo/components/auth/google-signin-handler.tsx
@@ -3,12 +3,24 @@
 import { createClient } from "@/utils/supabase/server";
 import { redirect } from "next/navigation";
 
+const getURL = () => {
+    let url =
+        process?.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
+        process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
+        "http://localhost:3000/";
+    // Make sure to include `https://` when not localhost.
+    url = url.startsWith("http") ? url : `https://${url}`;
+    // Make sure to include a trailing `/`.
+    url = url.endsWith("/") ? url : `${url}/`;
+    return url;
+};
+
 export default async function handleSignInWithGoogle() {
     const supabase = await createClient();
     const { data, error } = await supabase.auth.signInWithOAuth({
         provider: "google",
         options: {
-            redirectTo: "http://localhost:3000/auth/v1/callback",
+            redirectTo: getURL(),
         },
     });
 
